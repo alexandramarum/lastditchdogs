73f53684d0f15d0b932ac168b143b1fe3cf443ef
Alexandra Marum <alexandramarum@Alexandras-MacBook-Pro.local>
2025-06-22 13:14:42 -0400

    Update profiles

--- src/data/dogs.js
+++ src/data/dogs.js
@@ -11,7 +11,7 @@ export const dogs = [
         pfp: "/media/petey.jpeg",
         name: "Petey",
         description: "This beautiful boy is Petey whom I’ve been fostering for 6 months.  I think border collie hound mix.  About two years old and would do well in a family with older kids and another dog. He’s very obedient. He’s working on controlling his greeting enthusiasm - jumping.  He’s active but not a constant need.  He’s affectionate and wants LOVE.",
-        media: ["/media/petey.jpeg", "/media/petey1.jpg", "/media/petey2.jpg", "/media/petey3.jpg"]
+        media: ["/media/petey.jpeg", "/media/petey1.jpg", "/media/petey2.jpg", "/media/petey3.jpg", "/media/petey4.jpg", "/media/petey5.jpg", "/media/petey6.png"]
     },
     //  {
     //     id: 3,
@@ -25,20 +25,20 @@ export const dogs = [
         pfp: "/media/liampfp.jpg",
         name: "Liam",
         description: "",
-        media: ["/media/liampfp.jpg", "/media/liam1.jpg"]
+        media: ["/media/liampfp.jpg", "/media/liam1.jpg", "/media/liam2.jpg", "/media/liam3.png"]
     },
-     {
-        id: 5,
-        pfp: "/media/queeniepfp.jpg",
-        name: "Queenie",
-        description: "",
-        media: ["/media/queeniepfp.jpg", "/media/queenie1.jpg", "/media/queenie2.jpg"]
-    },
-     {
-        id: 6,
-        pfp: "/media/sampfp.JPG",
-        name: "Sam",
-        description: "",
-        media: ["/media/sampfp.JPG", "/media/sam1.jpg", "/media/sam2.jpg"]
-    }
+    //  {
+    //     id: 5,
+    //     pfp: "/media/queeniepfp.jpg",
+    //     name: "Queenie",
+    //     description: "",
+    //     media: ["/media/queeniepfp.jpg", "/media/queenie1.jpg", "/media/queenie2.jpg"]
+    // },
+    //  {
+    //     id: 6,
+    //     pfp: "/media/sampfp.JPG",
+    //     name: "Sam",
+    //     description: "",
+    //     media: ["/media/sampfp.JPG", "/media/sam1.jpg", "/media/sam2.jpg"]
+    // }
 ]
