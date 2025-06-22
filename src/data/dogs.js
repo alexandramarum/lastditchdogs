845bb836f482ed58d4d38595d59c80b4aacfb218
Alexandra Marum <alexandramarum@Alexandras-MacBook-Pro.local>
2025-06-22 12:40:21 -0400

    Update dog profiles

--- src/data/dogs.js
+++ src/data/dogs.js
@@ -1,25 +1,25 @@
 export const dogs = [
-    {
-        id: 1,
-        pfp: "/media/elliepfp.jpeg",
-        name: "Ellie",
-        description: "",
-        media: ["/media/elliepfp.jpeg", "/media/ellie1.jpg"]
-    },
+    // {
+    //     id: 1,
+    //     pfp: "/media/elliepfp.jpeg",
+    //     name: "Ellie",
+    //     description: "",
+    //     media: ["/media/elliepfp.jpeg", "/media/ellie1.jpg"]
+    // },
     {
         id: 2,
         pfp: "/media/petey.jpeg",
         name: "Petey",
-        description: "",
+        description: "This beautiful boy is Petey whom I’ve been fostering for 6 months.  I think border collie hound mix.  About two years old and would do well in a family with older kids and another dog. He’s very obedient. He’s working on controlling his greeting enthusiasm - jumping.  He’s active but not a constant need.  He’s affectionate and wants LOVE.",
         media: ["/media/petey.jpeg", "/media/petey1.jpg", "/media/petey2.jpg", "/media/petey3.jpg"]
     },
-     {
-        id: 3,
-        pfp: "/media/huskypfp.jpg",
-        name: "Husky",
-        description: "",
-        media: ["/media/huskypfp.jpg", "/media/husky1.jpg"]
-    },
+    //  {
+    //     id: 3,
+    //     pfp: "/media/huskypfp.jpg",
+    //     name: "Husky",
+    //     description: "",
+    //     media: ["/media/huskypfp.jpg", "/media/husky1.jpg"]
+    // },
      {
         id: 4,
         pfp: "/media/liampfp.jpg",
