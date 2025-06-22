86b5494a0f68e61498c645364fefe3fd21e605af
Alexandra Marum <alexandramarum@Alexandras-MacBook-Pro.local>
2025-06-22 13:16:50 -0400

    Fix formatting

--- src/data/dogs.js
+++ src/data/dogs.js
@@ -11,7 +11,7 @@ export const dogs = [
         pfp: "/media/petey.jpeg",
         name: "Petey",
         description: "This beautiful boy is Petey whom I’ve been fostering for 6 months.  I think border collie hound mix.  About two years old and would do well in a family with older kids and another dog. He’s very obedient. He’s working on controlling his greeting enthusiasm - jumping.  He’s active but not a constant need.  He’s affectionate and wants LOVE.",
-        media: ["/media/petey.jpeg", "/media/petey1.jpg", "/media/petey2.jpg", "/media/petey3.jpg", "/media/petey4.jpg", "/media/petey5.jpg", "/media/petey6.png"]
+        media: ["/media/petey.jpeg", "/media/petey1.jpg", "/media/petey2.jpg", "/media/petey3.jpg", "/media/petey4.jpg", "/media/petey5.jpg"]
     },
     //  {
     //     id: 3,
@@ -25,7 +25,7 @@ export const dogs = [
         pfp: "/media/liampfp.jpg",
         name: "Liam",
         description: "",
-        media: ["/media/liampfp.jpg", "/media/liam1.jpg", "/media/liam2.jpg", "/media/liam3.png"]
+        media: ["/media/liampfp.jpg", "/media/liam1.jpg", "/media/liam2.jpg",]
     },
     //  {
     //     id: 5,
